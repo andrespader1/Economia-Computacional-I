0962a0dc17b1f476290fcccd931114ebe2282378
andrespader1 <166852399+andrespader1@users.noreply.github.com>
2024-04-12 15:47:03 -0300

    Add files via upload

---
+++ Conversão m_mm001.py
@@ -0,0 +1,25 @@
+"""
+Programa Conversão de metros em milímetros
+Descrição: Este é um porgrama que lê um valor em metros e o converte para um valor em milímetros
+Autor: André Azevedo Spader
+Data: 29/03/2024
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+numero = 0
+constante = 1000
+multiplicação = 0
+
+# Entrada de dados
+
+numero = float(input("Digite o valor em metros: "))
+
+# Processamento de dados
+
+multiplicação = numero * constante
+
+# Saída de dados
+
+print(f"\nO valor {numero} metros corresponde a {multiplicação} milímetros.\n")
