0b8ef64b6f34dc6077f7d9baae1f72777995b4bf
andrespader1 <166852399+andrespader1@users.noreply.github.com>
2024-05-31 16:59:54 -0300

    Add files via upload

---
+++ 2024-1/Aula 2024 - 03 - 29/Soma/soma001.py
@@ -0,0 +1,29 @@
+"""
+Programa soma
+Descrição: Este programa lê dois números inteiros digitados pelo usuário e põe na tela a soma deles.
+Autor: André Azevedo Spader
+Data: 12/04/2024
+Versão: 0.0.1
+"""
+
+# Alocação de mmemória
+
+numero_1: int = 0
+
+numero_2: int = 0
+
+soma: int = 0
+
+# Entrada de dados
+
+numero_1 = int(input("Qual é o primeiro número? "))
+
+numero_2 = int(input("Qual é o segundo número? "))
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+# Saida de dados
+
+print(soma)
